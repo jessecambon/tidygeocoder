46c491f04a496da41d1e906999e70ce4271ebea1
Jesse Cambon <jesse.cambon@gmail.com>
2019-09-22 15:08:46 -0400

    vignette-init

--- R/geo_census.R
+++ R/geo_census.R
@@ -6,32 +6,33 @@
 #'
 #' @param address Single line address. Street must be included.
 #' @param verbose logical. If TRUE outputs logs.
-#' @return Latitude and Longitude Coordinates in Tibble format
-#'
-#' @examples
-#' geo_census("1600 Pennsylvania Ave Washington, DC")
-#' geo_census("600 Montgomery St, San Francisco, CA 94111")
-
+#' @param latitude name of latitude field
+#' @param longitude name of longitude field
+#' @param API_URL URL of Census API
 #' @importFrom httr GET content
 #' @importFrom tibble tibble
 #' @importFrom dplyr '%>%' mutate
 #' @importFrom jsonlite fromJSON
 #' @export
-geo_census <- function(address,verbose=FALSE){
+geo_census <- function(address,verbose=FALSE,latitude="lat",longitude="lng",
+                       API_URL="https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?"){
+  latitude <- rlang::enquo(latitude)
+  longitude <- rlang::enquo(longitude)
+
+
   benchmark <- 4
   if (verbose == TRUE) {
     print(address)
   }
   # API Call
-  base <- "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?"
-  soup <- httr::GET(url=base,query=list(address=address,format='json',benchmark=benchmark))
+  soup <- httr::GET(url=API_URL,query=list(address=address,format='json',benchmark=benchmark))
   dat <- jsonlite::fromJSON(httr::content(soup,as='text',encoding = "ISO-8859-1"), simplifyVector=TRUE)
 
   coords <- dat$result$addressMatches$coordinates
 
   # Return lat/lng in tibble form
   if (!is.null(coords)) {
-    tibble::tibble(lat=coords$y[1],lng=coords$x[1]) }
+    tibble::tibble(!!latitude:=coords$y[1],!!longitude:=coords$x[1]) }
   else {
-    tibble::tibble(lat=numeric(),lng=numeric()) }
+    tibble::tibble(!!latitude:=numeric(),!!longitude:=numeric()) }
 }
