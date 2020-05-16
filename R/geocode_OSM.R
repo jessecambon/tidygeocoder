8492df97d919ba8ccfea521946f7daa01d286fbc
Jesse Cambon <jesse.cambon@gmail.com>
2020-05-16 15:11:30 -0400

    missing-bb

--- R/geocode_OSM.R
+++ R/geocode_OSM.R
@@ -1,39 +1,39 @@
 #' geocode_OSM
 #'
-#' Geocodes a location using OpenStreetMap Nominatim. Reproduced from 
+#' Geocodes a location using OpenStreetMap Nominatim. Reproduced from
 #' mtennekes/tmaptools.
-#' 
+#'
 #' @references \url{https://github.com/mtennekes/tmaptools}
 #'
-#' Geocodes a location (based on a search query) to coordinates and a bounding box. 
-#' Similar to geocode from the ggmap package. It uses OpenStreetMap Nominatim. 
-#' For processing large amount of queries, please read the usage policy 
+#' Geocodes a location (based on a search query) to coordinates and a bounding box.
+#' Similar to geocode from the ggmap package. It uses OpenStreetMap Nominatim.
+#' For processing large amount of queries, please read the usage policy
 #' (\url{http://wiki.openstreetmap.org/wiki/Nominatim_usage_policy}).
 #'
-#' @param q a character (vector) that specifies a search query. 
+#' @param q a character (vector) that specifies a search query.
 #' For instance \code{"India"} or \code{"CBS Weg 11, Heerlen, Netherlands"}.
-#' @param projection projection in which the coordinates and bounding box 
-#' are returned. Either a \code{\link[sp:CRS]{CRS}} object or a character value. 
+#' @param projection projection in which the coordinates and bounding box
+#' are returned. Either a \code{\link[sp:CRS]{CRS}} object or a character value.
 #' If it is a character, it can either be a \code{PROJ.4} character string or
-#' a shortcut. See \code{\link{get_proj4}} for a list of shortcut values. 
+#' a shortcut. See \code{\link{get_proj4}} for a list of shortcut values.
 #' By default latitude longitude coordinates.
 #' @param return.first.only Only return the first result
 #' @param details provide output details, other than the point coordinates and bounding box
-#' @param as.data.frame Return the output as a \code{data.frame}. 
-#' If \code{FALSE}, a list is returned with at least two items: 
-#' \code{"coords"}, a vector containing the coordinates, 
-#' and \code{"bbox"}, the corresponding bounding box. 
-#' By default false, unless \code{q} contains multiple queries. 
+#' @param as.data.frame Return the output as a \code{data.frame}.
+#' If \code{FALSE}, a list is returned with at least two items:
+#' \code{"coords"}, a vector containing the coordinates,
+#' and \code{"bbox"}, the corresponding bounding box.
+#' By default false, unless \code{q} contains multiple queries.
 #' If \code{as.sf = TRUE} (see below), \code{as.data.frame} will set to \code{TRUE}.
-#' @param as.sf Return the output as \code{\link[sf:sf]{sf}} object. 
-#' If \code{TRUE}, \code{return.first.only} will be set to \code{TRUE}. 
-#' Two geometry columns are added: \code{bbox} and \code{point}. 
+#' @param as.sf Return the output as \code{\link[sf:sf]{sf}} object.
+#' If \code{TRUE}, \code{return.first.only} will be set to \code{TRUE}.
+#' Two geometry columns are added: \code{bbox} and \code{point}.
 #' The argument \code{geometry} determines which of them is set to the default geometry.
-#' @param geometry When \code{as.sf}, this arugment determines which column 
-#' (\code{bbox} or \code{point}) is set as geometry column. Note that the geometry 
+#' @param geometry When \code{as.sf}, this arugment determines which column
+#' (\code{bbox} or \code{point}) is set as geometry column. Note that the geometry
 #' can be changed afterwards with \code{\link[sf:st_set_geometry]{st_set_geometry}}.
 #' @param server OpenStreetMap Nominatim server name. Could also be a local OSM Nominatim server.
-#' @return If \code{as.SPDF} then a \code{\link[sp:SpatialPointsDataFrame]{SpatialPointsDataFrame}} 
+#' @return If \code{as.SPDF} then a \code{\link[sp:SpatialPointsDataFrame]{SpatialPointsDataFrame}}
 #' is returned. Else, if \code{as.data.frame}, then a \code{data.frame} is returned, else a list.
 #' @export
 #' @importFrom XML xmlChildren xmlRoot xmlAttrs xmlTreeParse xmlValue
@@ -226,13 +226,18 @@ rev_geocode_OSM <- function(x, y=NULL, zoom=NULL, projection=4326, as.data.frame
 		strzoom <- paste0(", zoom = ", zoom)
 	}
 
+
 	addr <- paste0(server, "/reverse?format=xml&lat=", lat, "&lon=", lon, qzoom, "&addressdetails=1")
 
 
 	dfs <- lapply(1:n, function(i) {
 		# download query
 		tmpfile <- tempfile()
+
+
 		suppressWarnings(download.file(addr[i], destfile = tmpfile, mode= "wb", quiet = TRUE))
+
+
 		doc <- xmlTreeParse(tmpfile, encoding="UTF-8")
 		unlink(tmpfile)
 
