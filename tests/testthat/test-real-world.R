e9f3db9df17f85e3740bcc45370d87aeec135475
twesleyb <twesleyb10@gmail.com>
2020-05-17 10:07:57 -0400

    work through a small example 100 rows

---
+++ tests/testthat/test-real-world.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+library(dplyr)
+library(tidygeocoder)
+
+# Project directories.
+here <- getwd()
+root <- dirname(dirname(here))
+
+# Load renv.
+renv::load(root)
+
+# Load the test data.
+datadir <- file.path(root,"data")
+myfile <- file.path(datadir,"durham_test.rda")
+load(myfile) # durham_test
+
+# Create column with addresses.
+df <- durham_test
+df$ADDR <- paste(trimws(df$SITE_ADDR),"Durham NC")
+
+# Encode addresses as lat/lon.
+df2 <- df %>% geocode(ADDR)
