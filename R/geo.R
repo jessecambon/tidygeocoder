e65bfb8ab970801249ac026c0e32489edcf9486b
Jesse Cambon <jesse.cambon@gmail.com>
2020-05-20 00:44:16 -0400

    geo-refactoring

--- R/geo.R
+++ R/geo.R
@@ -4,7 +4,6 @@
 
 # full_results : if true return all info provided by geocoder (not just lat/long)
 
-
 # Do not pass address if address = NULL ???
 
 ## method argument here will be used for:
@@ -15,67 +14,38 @@
 ## Set defaults for query parameters but allow user to override them. 
 ## Include address and limit as a query parameter.
 
-geo <- function(method,api_url=NULL, query_parameters=list(),
-                lat=lat, long=long, verbose=FALSE, min_time=NULL, full_results=FALSE) {
-  # what to return if address is invalid or no coordinates are found
-  NA_value <- tibble::tibble(!!lat := numeric(), !!long := numeric())
-  # lat/long variable names
-  lat <- rlang::enquo(lat)
-  long <- rlang::enquo(long)
+query_defaults <- list(
+  osm = list(format = 'json'),
+  census = list(format = 'json', benchmark = 4),
+  geocodio = list(),
+  iq = list()
+)
 
+# query_defaults['osm']
+
+geo <- function(method, api_url=NULL, query_parameters=list(), verbose=FALSE, min_time=NULL, full_results=FALSE) {
+  start_time <- Sys.time() # start timer
+  
   #### SET API URL BASED ON SERVICE IF IT IS NULL
   
   ### IF min_time is NULL then set it to 1 if method='osm', otherwise set to 0
-  if (method == 'osm' & is.null(min_time)) {
-    min_time <- 1
-  } else {
-    min_time <- 0
-  }
+  if (method == 'osm' & is.null(min_time))  min_time <- 1 else min_time <- 0
+
+
+  ## Call Geocoder Service
+  res <- get_raw_results(api_url, query_parameters)
   
-  ### If query
-  
-  
-  start_time <- Sys.time() # start timer
-  
-  if (verbose == TRUE) { message(address)}
-  
-  # if address is NA or blank then return NA, else make call to Nominatim geocoder
-  # numeric data is allowed but must be in string format (ie. for zip codes)
-  if (is.na(address) | stringr::str_trim(address) == "") {
-    if (verbose == TRUE) { message("Blank or missing address!") }
+  # If no results found, return NA
+  if (length(res) == 0) {
+    if (verbose == TRUE) {  message("No results found") }
     return(NA_value)
   }
-    
-    ## Call Geocoder Service
-    
-    res <- get_raw_results(api_url, list(q = address, format = 'json', limit = 1 ))
-    
-    # If no results found, return NA
-    if (length(res) == 0) {
-      if (verbose == TRUE) {  message(paste("No results found for \"", address, "\".", sep = "")) }
-      return(NA_value)
-    }
-    
-    ## Parse geocoder results
-    
-    
-    
-    ## Make sure the proper amount of time has elapsed for the query per min_time
-    
-    seconds_elapsed <- as.numeric(difftime(Sys.time(), start_time, units = 'secs'))
-    
-    if (debug == TRUE) message(paste0('Time elapsed: ', round(seconds_elapsed,1),' seconds'))
-    
-    # Sleep if necessary to make query take the minimum amount of time
-    if (seconds_elapsed < min_time) {
-      Sys.sleep(min_time - seconds_elapsed)
-      
-      if (debug == TRUE) message(paste0('Time elapsed (after sleep): ', 
-                                        round(as.numeric(difftime(Sys.time(),start_time, units = 'secs')),1),' seconds'))
-    }
-    
-    # Return  results
-    if (!is.null(coords)) return(tibble::tibble(!!lat := coords[1], !!long := coords[2]))
-    else return(NA_value)
+  
+  ## Parse geocoder results
+  
+  ## Make sure the proper amount of time has elapsed for the query per min_time
+  pause_until(start_time, min_time,debug = debug) 
+  
+  ### Return  results
   
 }
--- R/utils.R
+++ R/utils.R
@@ -17,3 +17,24 @@ get_raw_results <- function(api_url, query_parameters, content_encoding='UTF-8')
     jsonlite::fromJSON(httr::content(response, as = 'text', encoding = content_encoding))
   )
 }
+
+# How many seconds have elapsed since start time t0 (as defined by a t0 <- Sys.time() call) 
+get_seconds_elapsed <- function(t0) {
+  return(
+  as.numeric(difftime(Sys.time(), t0, units = 'secs'))
+  )
+}
+
+pause_until <- function(start_time,min_time,debug=FALSE) {
+  
+  ## Make sure the proper amount of time has elapsed for the query per min_time
+  seconds_elapsed <- get_seconds_elapsed(start_time)
+  if (debug == TRUE) message(paste0('Time elapsed: ', round(seconds_elapsed,1),' seconds'))
+  
+  # Sleep if necessary to make query take the minimum amount of time
+  if (seconds_elapsed < min_time) {
+    Sys.sleep(min_time - seconds_elapsed)
+    if (debug == TRUE) message(paste0('Time elapsed (after sleep): ', 
+                                      round(get_seconds_elapsed(start_time),1),' seconds'))
+  }
+}
