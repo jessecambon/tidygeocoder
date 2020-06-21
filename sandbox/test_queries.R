ce7e136964f3e24076044fd45a07594cce136361
Jesse Cambon <jesse.cambon@gmail.com>
2020-06-21 14:52:23 -0400

    fix-test-queries

--- sandbox/test_queries.R
+++ sandbox/test_queries.R
@@ -3,11 +3,11 @@ test2 <- geo(address = '11 Wall St New York, NY')
 test3 <- geo(address = '11 Wall St New York, NY')
 test4 <- geo(street = c('1600 Pennsylvania Ave NW', '11 Wall Street'), city = c('Washington', 'New York'), state = c('DC', 'NY'), verbose = TRUE)
 
-census_geo1 <- test1 <- geo(address = c('11 Wall St New York, NY', '1600 Pennsylvania Ave NW Washington, DC', '11 Wall St New York, NY'), 
-                  verbose = TRUE, full_results = TRUE, return = 'geographies')
+census_geo1  <- geo(address = c('11 Wall St New York, NY', '1600 Pennsylvania Ave NW Washington, DC', '11 Wall St New York, NY'), 
+                  verbose = TRUE, full_results = TRUE, return_type = 'geographies')
 
-census_geo2 <- test1 <- geo(address = c('11 Wall St New York, NY', '1600 Pennsylvania Ave NW Washington, DC', '11 Wall St New York, NY'), 
-      verbose = TRUE, full_results = TRUE, return = 'locations', unique_only = TRUE)
+census_geo2  <- geo(address = c('11 Wall St New York, NY', '1600 Pennsylvania Ave NW Washington, DC', '11 Wall St New York, NY'), 
+      verbose = TRUE, full_results = TRUE, return_type = 'locations', unique_only = TRUE)
 
 ## Batch Geocodio 
 geocodio_batch <- geo(address = c('11 Wall St New York, NY', '1600 Pennsylvania Ave NW Washington, DC',
