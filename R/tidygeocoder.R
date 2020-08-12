ac7dc28b50d9d20ce309650c21b323bcdba86c72
Jesse Cambon <jesse.cambon@gmail.com>
2020-08-12 17:14:50 -0400

    cran-tweaks

--- R/tidygeocoder.R
+++ R/tidygeocoder.R
@@ -1,8 +1,9 @@
 #' \pkg{tidygeocoder} makes getting data from geocoder services easy. 
 #'
 #' The primary functions to use are \code{\link{geocode}} and \code{\link{geo}}.
-#' The \code{\link{geocode}} function calls \code{\link{geo}}.
-#' See \code{vignette("tidygeocoder")} for details.
+#' Refer to the documentation on the method argument of the \code{\link{geo}} function
+#' for more details on the available geocoder services. Also see \code{vignette("tidygeocoder")} 
+#' for example usage.
 
 #' @keywords internal
 "_PACKAGE"
--- cran-comments.md
+++ cran-comments.md
@@ -3,8 +3,12 @@
 * winbuilder r-old devel: `devtools::check_win_oldrelease()`
 * winbuilder r-devel : `devtools::check_win_devel()`
 * Other environments checked via `rhub::check_for_cran(env_vars=c(R_COMPILE_AND_INSTALL_PACKAGES = "always"))`
-```
 
 ## R CMD check results
 
 0 errors | 0 warnings | 0 notes
+
+## Notes 
+
+Per CRAN request, updated two URLs to include trailing "/" and highlighted the geocoder service
+options available in R/tidygeocoder.R.
