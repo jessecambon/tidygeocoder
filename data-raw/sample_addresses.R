568b54d824cb231e7fba879fea184eed7b1e6e42
Jesse Cambon <jesse.cambon@gmail.com>
2021-01-16 16:36:09 -0500

    final-final-1-0-2

--- data-raw/sample_addresses.R
+++ data-raw/sample_addresses.R
@@ -3,7 +3,7 @@
 # Small address dataset for testing
 sample_addresses <- tibble::tribble( 
   ~name,                         ~addr,
-   "White House",                "1600 Pennsylvania Ave Washington, DC",
+   "White House",                "1600 Pennsylvania Ave NW Washington, DC",
    "Transamerica Pyramid",       "600 Montgomery St, San Francisco, CA 94111",
    "NY Stock Exchange",          "11 Wall Street, New York, New York",
    "Willis Tower",               "233 S Wacker Dr, Chicago, IL 60606",
@@ -14,14 +14,14 @@ sample_addresses <- tibble::tribble(
    "Tokyo",                      "Tokyo, Japan",
 )
 
-# Invalid/blank addresses for testing ---- move to tests folder
-junk_addresses <- tibble::tribble( 
-  ~name,               ~addr,
-  'NA',                NA,
-  'Null',              NULL,
-  "Blank",             "",
-  "White Space",       "      ",
-)
+## Invalid/blank addresses for testing ---- move to tests folder
+# junk_addresses <- tibble::tribble( 
+#   ~name,               ~addr,
+#   'NA',                NA,
+#   'Null',              NULL,
+#   "Blank",             "",
+#   "White Space",       "      ",
+# )
 
 usethis::use_data(sample_addresses, overwrite = TRUE)
 #usethis::use_data(invalid_addresses, overwrite = TRUE)
