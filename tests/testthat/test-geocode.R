7c15654d2434c8982c82a225946c1211ad6bf561
dieghernan <diego.hernangomezherrero@gmail.com>
2021-03-08 08:42:27 +0100

    Prepare CI testing
    To be restored before PR

--- tests/testthat/test-geocode.R
+++ tests/testthat/test-geocode.R
@@ -105,7 +105,8 @@ test_that("Test geocode() error handling", {
 # Check that null/empty address values are handled properly
 test_that("reverse geocode null/empty addresses", {
   NA_result <- tibble::tibble(address = as.character(NA))
-  
+  skip_on_cran()
+  skip_on_ci()
   # make sure blank addresses are not being sent to the geocoder
   expect_identical(reverse_geo(lat = " ", long = " ", method = 'osm', return_coords = FALSE, no_query = TRUE), NA_result)
   expect_identical(reverse_geo(lat =" ", long = " ", method = 'google', return_coords = FALSE, no_query = TRUE), NA_result)
