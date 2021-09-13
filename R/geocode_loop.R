83d2e83721705d83b29e4c67e64b5eff73cd80f0
Jesse Cambon <jesse.cambon@gmail.com>
2021-09-12 20:47:17 -0400

    working geocode_loop function

--- R/geocode_loop.R
+++ R/geocode_loop.R
@@ -3,19 +3,22 @@
 #' New function to replace method = 'cascade'
 #'
 #' @export
-geocode_loop <- function(.tbl, queries, common_params = list(), lat = lat, long = long, stack = TRUE, cascade = FALSE) {
-  # NSE eval ('lat' and lat are both accepted)
+geocode_loop <- function(.tbl, queries, common_params = list(), stack = TRUE, cascade = TRUE, lat = 'lat', long = 'long') {
+  
   lat <- rm_quote(deparse(substitute(lat)))
   long <- rm_quote(deparse(substitute(long)))
+  
+  # TODO: throw error if cascade == TRUE and return_addresses == FALSE (check all list args passed)
+  # since we need addresses to know what was or wasn't found
+  
+  # TODO: add preemptive checking of parameters by running all the queries with no_query = TRUE and a tryCatch
+  
+  # TODO: add a column for stacked results that shows which data is from which query
   
   # add common arguments to each query
   queries_prepped <- lapply(queries, function(x) {
     c(
-      list(
-        .tbl = .tbl,
-        lat = lat,
-        long = long
-        ),
+      list(.tbl = .tbl),
         common_params,
       x)})
   
@@ -23,12 +26,23 @@ geocode_loop <- function(.tbl, queries, common_params = list(), lat = lat, long 
   # iterate through the queries (list of lists) and execute each query
   # aggregate results in list object
   all_results <- list()
+  not_found <- tibble::tibble()
   for (query in queries_prepped) {
+    
+    if (cascade == TRUE) {
+      # adjust the input dataframe based on which addresses were not found in prior query
+      if (nrow(not_found) != 0) {
+        query[['.tbl']] <- not_found
+      }
+    }
     
     result <- do.call(geocode, query)
     
     # which addresses were not found
-    #na_indices <- is.na(result[[lat]]) | is.na(result[[long]])
+    if (cascade == TRUE) {
+      #distinct_found <- dplyr::distinct(result[names(result) %in% intersect(names(query), pkg.globals$address_arg_names]))
+      not_found <- result[is.na(result[[lat]]) | is.na(result[[long]]), intersect(colnames(result), colnames(.tbl))]
+    }
     
     all_results <- c(all_results, list(result)) 
   }
@@ -42,3 +56,5 @@ geocode_loop <- function(.tbl, queries, common_params = list(), lat = lat, long 
   }
 
 }
+
+# geocode_loop(sample_addresses, list(list(method = 'census'), list(method = 'osm')), list(address = 'addr'), cascade = TRUE)
