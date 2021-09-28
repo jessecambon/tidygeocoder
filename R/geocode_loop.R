8264123f9386061646bfb2a61211f4ead7540ece
Jesse Cambon <jesse.cambon@gmail.com>
2021-09-28 17:47:26 -0400

    geocode_loop now separates results properly

--- R/geocode_loop.R
+++ R/geocode_loop.R
@@ -5,13 +5,13 @@
 #' 
 #' @param queries list of lists parameter. Each list contains parameters for a query
 #'   (ie. `list(list(method = 'osm'), list(method = 'census'), ...)`)
-#' @param common_params list parameter. Contains arguments that should be used for all queries.
+#' @param global_params list parameter. Contains arguments that should be used for all queries.
 #'   (ie. `list(full_results = TRUE, unique_only = TRUE)`)
 #' @inheritParams geo
 #' @param ... arguments passed to the [geocode_loop] function
 #' @inherit geo return
 #' @export
-geo_loop <- function(queries, common_params = list(), address = NULL, 
+geo_loop <- function(queries, global_params = list(), address = NULL, 
                      street = NULL, city = NULL, county = NULL, state = NULL, postalcode = NULL, country = NULL, ...) {
   
   # Check argument inputs
@@ -23,14 +23,14 @@ geo_loop <- function(queries, common_params = list(), address = NULL,
     street = street, city = city, county = county, state = state, postalcode = postalcode, country = country
   )
   
-   common_params_combi <- c(
-     common_params, 
+   global_params_combi <- c(
+     global_params, 
      list(address = address, street = street, city = city, county = county,
            state = state, postalcode = postalcode, country = country)
      )
    
   return(
-    geocode_loop(.tbl = input_df, common_params = common_params_combi, ...)
+    geocode_loop(.tbl = input_df, global_params = global_params_combi, ...)
   )
 }
 
@@ -41,7 +41,7 @@ geo_loop <- function(queries, common_params = list(), address = NULL,
 #' 
 #' @param queries list of lists parameter. Each list contains parameters for a query
 #'   (ie. `list(list(method = 'osm'), list(method = 'census'), ...)`)
-#' @param common_params list parameter. Contains arguments that should be used for all queries.
+#' @param global_params list parameter. Contains arguments that should be used for all queries.
 #'   (ie. `list(full_results = TRUE, unique_only = TRUE)`)
 #' @param stack if TRUE then a single dataframe will be returned. When stack = FALSE
 #'   then a list of dataframes, one per each query, will be returned.
@@ -51,21 +51,26 @@ geo_loop <- function(queries, common_params = list(), address = NULL,
 #' @inheritParams geocode
 #' @inherit geo return
 #' @export
-geocode_loop <- function(.tbl, queries, common_params = list(), query_names = NULL, stack = TRUE, cascade = TRUE, lat = lat, long = long) {
+geocode_loop <- function(.tbl, queries, global_params = list(), query_names = NULL, stack = TRUE, cascade = TRUE, lat = lat, long = long) {
   
   lat <- rm_quote(deparse(substitute(lat)))
   long <- rm_quote(deparse(substitute(long)))
   
   # TODO: throw error if cascade == TRUE and return_addresses == FALSE (check all list args passed)
-  # since we need addresses to know what was or wasn't found
+  # since we need addresses to know what was or wasn't 
+  
+  # TODO: add a 'not found' column/item in the list that is returned to separate out
+  # the addresses that are not found
   
   # TODO: add preemptive checking of parameters by running all the queries with no_query = TRUE and a tryCatch
   
-  # add common arguments to each query
+  # TODO: order the results properly
+  
+  # add global arguments to each query
   queries_prepped <- lapply(queries, function(x) {
     c(
       list(.tbl = .tbl),
-        common_params,
+        global_params,
       x)})
   
   # Set default query names and check user input query names
@@ -73,7 +78,7 @@ geocode_loop <- function(.tbl, queries, common_params = list(), query_names = NU
     query_names <- unlist(lapply(queries_prepped, function(q) q[['method']]))
   } else {
     if (length(query_names) != length(queries)) {
-      stop('query_names parameter have one name per query. see ?geocode_loop')
+      stop('query_names parameter must contain one name per query provided. see ?geocode_loop')
     }
   }
   
@@ -81,7 +86,9 @@ geocode_loop <- function(.tbl, queries, common_params = list(), query_names = NU
   # aggregate results in list object
   all_results <- list()
   not_found <- tibble::tibble()
-  for (query in queries_prepped) {
+  for (i in 1:length(queries_prepped)) {
+    
+    query <- queries_prepped[[i]]
     
     if (cascade == TRUE) {
       # adjust the input dataframe based on which addresses were not found in prior query
@@ -98,7 +105,13 @@ geocode_loop <- function(.tbl, queries, common_params = list(), query_names = NU
       not_found <- result[is.na(result[[lat]]) | is.na(result[[long]]), intersect(colnames(result), colnames(.tbl))]
     }
     
-    all_results <- c(all_results, list(result)) 
+    # only return non-NA results unless we are at the last query in the loop
+    result_to_return <- if (i == length(queries_prepped)) result else {
+      result[!is.na(result[[lat]]) & !is.na(result[[long]]), ]
+    }
+    
+    # aggregate results
+    all_results <- c(all_results, list(result_to_return)) 
   }
   
   names(all_results) <- query_names
@@ -119,4 +132,4 @@ geocode_loop <- function(.tbl, queries, common_params = list(), query_names = NU
 
 }
 
-# geocode_loop(sample_addresses, list(list(method = 'census'), list(method = 'osm')), list(address = 'addr'), cascade = TRUE)
+# a <- geocode_loop(sample_addresses, list(list(method = 'census'), list(method = 'osm')), list(address = 'addr'), cascade = TRUE)
